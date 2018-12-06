6cea682deb045ae2e0acffc9357c8884a8545666
cirjeffrey <cirjeffrey92@gmail.com>
2018-12-05 16:56:42 -0800

    starter code
    uses getline to get word up until given parameter
    
    also uses append and insert to add char to beginning and end respectively

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// Example program
+#include <iostream>
+#include <string>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+using namespace std;
+
+int main()
+{
+    
+
+string token;
+string first, second;
+
+string str;
+
+getline(cin,first, ' ');
+first.append("x");
+first.insert(0,"x");
+cout << first;
+
+
+
+
+}
