d7a21a8eb11c33095124bbd16a3409d16c32edbd
Simon1091 <simonlee839@gmail.com>
2018-12-09 11:31:30 -0800

    part 2

--- Source1.cpp
+++ Source1.cpp
@@ -9,93 +9,120 @@ void showStack(stack<string> s);
 int getRow(string r);
 int getCol(string c);
 // "program B ; var F begin J end"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",
-bool isAccepted(const std::string *w, size_t length, stack<string>s) {
+bool isAccepted(vector<string> w, size_t length, stack<string>s) {
 	string table[22][32] = {
 		//	P	V	B	EN	I	S	A	B	C	D	E	0	1	2	3	4	5	6	7	8	9	+	-	*	/	(	)	=	$	:	;	,
-			"program B ; var F begin J end","", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", //A
-			"", "", "", "", "", "", "C D", "C D", "C D", "C D", "C D", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", //B
-			"", "", "", "", "", "", "a", "b", "c", "d", "e", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//C
-			"", "", "", "", "", "", "C D", "C D", "C D", "C D", "C D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "L", "L", "L", "L", "", "L", "L", "", "L", "L", "L",//D
-			"", "", "", "", "", "", "", "", "", "", "", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "", "", "", "", "", "", "", "", "", "", "",//E
-			"", "", "", "", "", "", "G : I ;", "G : I ;", "G : I ;", "G : I ;", "G : I ;", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//F
-			"", "", "", "", "", "", "G , H", "G , H", "G , H", "G , H", "G , H", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//G
-			"", "", "", "", "", "", "G", "G", "G", "G", "G", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//H
-			"", "", "", "", "integer", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//I
-			"", "", "", "", "", "K L", "K L", "K L", "K L", "K L", "K L", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//J
-			"", "", "", "", "", "M", "N", "N", "N", "N", "N", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//K
-			"", "", "", "", "", "J", "J", "J", "J", "J", "J", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//L
-			"", "", "", "", "", "show", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//M
-			"", "", "", "", "", "", "B = O ;", "B = O ;", "B = O ;", "B = O ;", "B = O ;", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//N
-			"", "", "", "", "", "", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "", "", "P Q", "L", "", "", "", "L", "",//O
-			"", "", "", "", "", "", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "L", "L", "", "", "", "L", "", "", "", "L", "",//P
-			"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "+ P Q", "- P Q", "", "", "", "", "", "", "", "", "",//Q
-			"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "L", "L", "* S R", "* S R", "", "L", "", "", "", "L", "",//R
-			"", "", "", "", "", "", "B", "B", "B", "B", "B", "T", "T", "T", "T", "T", "T", "T", "T", "T", "T", "", "", "", "", "( O )", "", "", "", "", "", "",//S
-			"", "", "", "", "", "", "", "", "", "", "", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "", "", "", "", "", "", "", "", "",
-			"", "", "", "", "", "", "", "", "", "", "", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "L", "L", "L", "L", "", "", "", "", "", "L", "",//U
-			"", "", "", "", "", "", "", "", "", "", "", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "+", "-", "", "", "", "", "", "", "", "", "", };//V
-	int i = 0; //index for iterating through the word
+		"program B ; var F begin J end","", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", //A
+		"", "", "", "", "", "", "C D", "C D", "C D", "C D", "C D", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", //B
+		"", "", "", "", "", "", "a", "b", "c", "d", "e", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//C
+		"", "", "", "", "", "", "C D", "C D", "C D", "C D", "C D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "E D", "L", "L", "L", "L", "", "L", "L", "", "L", "L", "L",//D
+		"", "", "", "", "", "", "", "", "", "", "", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "", "", "", "", "", "", "", "", "", "", "",//E
+		"", "", "", "", "", "", "G : I ;", "G : I ;", "G : I ;", "G : I ;", "G : I ;", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//F
+		"", "", "", "", "", "", "B , H", "B , H", "B , H", "B , H", "B , H", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//G
+		"", "", "", "", "", "", "G", "G", "G", "G", "G", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//H
+		"", "", "", "", "integer", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//I
+		"", "", "", "", "", "K W", "K W", "K W", "K W", "K W", "K W", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//J
+		"", "", "", "", "", "M", "N", "N", "N", "N", "N", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//K
+		"", "", "", "", "", "J", "J", "J", "J", "J", "J", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//L
+		"", "", "", "", "", "show", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//M
+		"", "", "", "", "", "", "B = O ;", "B = O ;", "B = O ;", "B = O ;", "B = O ;", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",//N
+		"", "", "", "", "", "", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "P Q", "", "", "P Q", "L", "", "", "", "L", "",//O
+		"", "", "", "", "", "", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "S R", "L", "L", "", "", "", "L", "", "", "", "L", "",//P
+		"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "+ P Q", "- P Q", "", "", "", "", "", "", "", "", "",//Q
+		"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "L", "L", "* S R", "* S R", "", "L", "", "", "", "L", "",//R
+		"", "", "", "", "", "", "B", "B", "B", "B", "B", "T", "T", "T", "T", "T", "T", "T", "T", "T", "T", "", "", "", "", "( O )", "", "", "", "", "", "",//S
+		"", "", "", "", "", "", "", "", "", "", "", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "V E U", "", "", "", "", "", "", "", "", "",
+		"", "", "", "", "", "", "", "", "", "", "", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "E U", "L", "L", "L", "L", "", "", "", "", "", "L", "",//U
+		"", "", "", "", "", "", "", "", "", "", "", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "+", "-", "", "", "", "", "", "", "", "", "", };//V
+	//int i = 0; //index for iterating through the word
 	string t; //the popped char
 	int r; //the row number
 	int c; // the column number
 	string e; //the string in specified index from table
-
+	vector<string> reserved_words = { "program","var","begin","show","end","integer" };
+	string h;
+	bool is_reserved;
 	s.push("$");
 	s.push("A");
 	t = s.top();
 	s.pop();
 	//this while loop will never finish because the function returns true or false
 	//once it matches $ or " " during each iteration
-	while (i < length) {
-
-		int r = getRow(t);
-		int c = getCol(w[i]);
-		e = table[r][c];
-		if (t == "$") //accepted
-			return true;
-
-		else if (t == w[i]) { //match
-			showStack(s);
-			cout << t << " == " << w[i] << "match" << endl;
-			t = s.top();
-			s.pop();
-			i++;
-		}
-		else if (e == " ") { //rejected
-			return false;
-		}
-		else if (e == "L")
-		{
-			t = s.top();
-			s.pop();
-		}
-
-		else {
-			std::string str = e;
-			std::istringstream buf(str);
-			std::istream_iterator<std::string> beg(buf), end;
-
-			std::vector<std::string> tokens(beg, end); // done!
-
-			for (auto& s : tokens)
-				std::cout << '"' << s << '"' << '\n';
-			for (int j = tokens.size() - 1; j >= 0; j--) {
-				s.push(tokens[j]);
+	int num_of_commas = 0;
+	int curr_comma = 1;
+	for (int k = 0; k < w.size(); k++) {
+		if (w[k] == ",")
+			num_of_commas++;
+	}
+	for (int j = 0; j < length; j++) {
+		int i = 0;
+		int token_size = w[j].size();
+		
+		for (int z = 0; z < reserved_words.size(); z++) {
+			if (w[j] == reserved_words[z]) {
+				cout << "RESERVED WORDsubstr: " << w[j].substr(i, w[j].size());
+				token_size = 1;
+				
 			}
-			
-			showStack(s);
-			t = s.top();
-			s.pop();
+		}
+		while (i < token_size) {
+			int r = getRow(t);
+			int c = getCol(w[j].substr(i, token_size));
+			e = table[r][c];
+			if (t == ",") {
+				
+				curr_comma++;
+				cout << "curr_comma " << curr_comma << endl;
+			}
+			if (t == "H") {
+				cout << "change G to B" << endl;
+				if (curr_comma == num_of_commas) {
+					e = "B";
+				}
+			}
+			if (t == "$") {
+				return true;
+			}
+			else if (t == w[j].substr(i, token_size)) {
+				cout << "stack before popping after match" << endl;
+				showStack(s);
+				t = s.top();
+				s.pop();
+				i++;
+			}
+			else if (e == "") { //rejected
+				return false;
+			}
+			else if (e == "L")
+			{
+				cout << "lambda at row " << r << "col " << c << endl;
+				t = s.top();
+				s.pop();
+			}
+			else {
+				cout << "pushing at row " << r << "col " << c << endl;
+				std::string str = e;
+				std::istringstream buf(str);
+				std::istream_iterator<std::string> beg(buf), end;
+
+				std::vector<std::string> tokens(beg, end); // done!
+
+				for (auto& s : tokens)
+					std::cout << '"' << s << '"' << '\n';
+				for (int j = tokens.size() - 1; j >= 0; j--) {
+					s.push(tokens[j]);
+				}
+				cout << "stack after pushing e" << endl;
+				showStack(s);
+				t = s.top();
+				s.pop();
 
 
+			}
 		}
 	}
 
-
-
 }
-
-
 
 
 
@@ -105,50 +132,21 @@ void showStack(stack<string> s) {
 
 }
 int main() {
-	string arr[] = { "program", "a2018",";", "var", "ab1" ,",", "cd" ,",", "e33a", "," ,"d18", ":", "integer", ";", "begin",
-	"abl","=","33",";","cd","-","41","e33a","=","5",";","show","(","ab1", ")",";","end"};
-		/*ab1 = 33;
-	cd - 41;
-	e33a = 5;
-	show(ab1);
-	d18 = ab1 * (cd + 2 * e33a);
-	show(d18);
-	end" }; */
-		stack<string> s;
-		int size = sizeof(arr) / sizeof(arr[0]);
-		isAccepted(arr, size, s);
-	/*for (int i = 0; i < 5; ++i)
-	{
-	for (int j = 0; j < 8 ; ++j)
-	{
-	std::cout << "row" << i <<"col"<<j<< table[i][j] << ' ';
+	vector<string> arr = { "program", "a2018",";", "var", "ab1" ,",", "cd" ,",", "e33a", "," ,"d18", ":", "integer", ";", "begin",
+		"abl","=","33",";","cd","-","41","e33a","=","5",";","show","(","ab1", ")",";","end" };
+	stack<string> s;
+	vector<string> reserved_words = { "program","var","begin","show","end","integer" };
+	int i = 0;
+	int token_size = arr[i].size();
+	cout << token_size << "tokensize" << endl;
+	for (int z = 0; z < reserved_words.size(); z++) {
+		if (arr[i] == reserved_words[z]) {
+			cout << "RESERVED WORDsubstr: " << arr[i].substr(i, arr[i].size());
+			token_size = 1;
+
+		}
 	}
-	std::cout << std::endl;
-	}*/
-	/*for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-		stack<string> s;
-		if (isAccepted(arr[i], s)) {
-			cout << "--------------------------" << endl;
-			cout << i + 1 << "." << arr[i];
-			cout << "accepted" << endl;
-			cout << "--------------------------" << endl;
-		}
-		else {
-			cout << "--------------------------" << endl;
-			cout << i + 1 << "." << arr[i];
-			cout << "rejected" << endl;
-			cout << "--------------------------" << endl;
-
-		}
-
-	}*/
-
-
-
-
-
-
-
+	//isAccepted(arr, arr.size(), s);
 	system("pause");
 	return 0;
 }
@@ -165,7 +163,7 @@ int getRow(string r) { //get row number associated with non-terminal
 	else if (r == "I") { x = 8; }
 	else if (r == "J") { x = 9; }
 	else if (r == "K") { x = 10; }
-	else if (r == "L") { x = 11; }
+	else if (r == "W") { x = 11; }
 	else if (r == "M") { x = 12; }
 	else if (r == "N") { x = 13; }
 	else if (r == "O") { x = 14; }
@@ -177,27 +175,6 @@ int getRow(string r) { //get row number associated with non-terminal
 	else if (r == "U") { x = 20; }
 	else if (r == "V") { x = 21; }
 
-	/*switch (r[0]) {
-	case 'S':
-		x = 0;
-		break;
-	case 'E':
-		x = 1;
-		break;
-	case 'T':
-		x = 3;
-		break;
-	case 'F':
-		x = 5;
-		break;
-	case 'R':
-		x = 4;
-		break;
-	case 'Q':
-		x = 2;
-	default:
-		break; 
-	}*/
 	return x;
 }
 int getCol(string c) { //get the column number associated with terminal
@@ -234,37 +211,5 @@ int getCol(string c) { //get the column number associated with terminal
 	if (c == ":") { y = 29; }
 	if (c == ";") { y = 30; }
 	if (c == ",") { y = 31; }
-	
-
-	/*switch (c) {
-	case 'b':
-		y = 8;
-		break;
-	case '(':
-		y = 5;
-		break;
-	case ')':
-		y = 6;
-		break;
-	case 'a':
-		y = 0;
-		break;
-	case '+':
-		y = 1;
-		break;
-	case '-':
-		y = 2;
-		break;
-	case '*':
-		y = 3;
-		break;
-	case '/':
-		y = 4;
-		break;
-	case '$':
-		y = 7;
-	default:
-		break;
-	}*/
 	return y;
 }
